03bb9284d58a1cf0d5569ee86c982952cc79d22e
Kevin_Ibanez <kevin.ibanezpro@gmail.com>
2020-02-02 18:06:51 +0100

    laser

--- Assets/Scripts/Script_BurgerDeadZone.cs
+++ Assets/Scripts/Script_BurgerDeadZone.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Script_BurgerDeadZone : MonoBehaviour
 {
+    public GameObject laser;
+    public AudioSource audioComp;
+    public AudioClip laserSound;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.GetComponentInParent<Script_Burger>().type != BurgerType.good)
         {
             Script_GameManager.Instance.SubstractLife(1);
+            Script_GameManager.Instance.RemoveBurgerFromList(other.gameObject);
+            other.gameObject.GetComponentInParent<Script_Burger>().DestroyMe();
+            laser.SetActive(true);
+            audioComp.PlayOneShot(laserSound);
+            StartCoroutine(WaitToUnshowLaser());
         }
+    }
 
-        Script_GameManager.Instance.RemoveBurgerFromList(other.gameObject);
-        other.gameObject.GetComponentInParent<Script_Burger>().DestroyMe();
+    IEnumerator WaitToUnshowLaser()
+    {
+        yield return new WaitForSeconds(0.2f);
+        laser.SetActive(false);
     }
 }
